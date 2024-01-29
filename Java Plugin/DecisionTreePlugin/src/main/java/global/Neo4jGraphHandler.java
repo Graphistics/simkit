72c4f2030e3af1e0a7f81688a8006e3bfd2fda4a
abhivanth <abhivanthmurali@outlook.com>
2024-01-29 22:37:46 +0100

    refactor : added method to avoid merge conflict

--- Java Plugin/DecisionTreePlugin/src/main/java/global/Neo4jGraphHandler.java
+++ Java Plugin/DecisionTreePlugin/src/main/java/global/Neo4jGraphHandler.java
@@ -77,8 +77,29 @@ public class Neo4jGraphHandler {
         }
         return nodeList;
     }
+    public static ArrayList<NodeList2> retrieveNodeListFromNeo4j(final String nodeType, Driver driver) {
+        ArrayList<NodeList2> nodeList = new ArrayList<>();
 
+        try (Session session = driver.session()) {
+            String cypherQuery = "MATCH (n:" + nodeType + ") RETURN n, n.id AS index";
+            Result result = session.run(cypherQuery);
 
+            while (result.hasNext()) {
+                Record record = result.next();
+                Node node = record.get("n").asNode();
+
+                String index = record.get("index").asString();
+
+                Map<String, Object> nodeProperties = extractPropertiesFromNode(node);
+
+                NodeList2 nodeObject = new NodeList2(index, nodeProperties);
+                nodeList.add(nodeObject);
+            }
+        } catch (Neo4jException e) {
+            throw new RuntimeException("Error retrieving node data from Neo4j for label: " + nodeType + ", Error: " + e.getMessage());
+        }
+        return nodeList;
+    }
     public static void createNodeGraph(String graphType, String message, NodeList2 nodeDetail, Driver driver) {
         final String id = nodeDetail.getIndex();
         final Map<String, Object> properties = nodeDetail.getProperties();
